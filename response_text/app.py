ad3ef4865c34811313599dadb3873d7755e1593d
ymd65536 <ymd65536@gmail.com>
2024-03-06 09:20:56 +0900

    modified: slack reply

--- response_text/app.py
+++ response_text/app.py
@@ -3,11 +3,11 @@ import os
 from slack_bolt import App, Ack
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
+project_id = os.environ.get("PROJECT_ID", "")
 APP_ENVIRONMENT = os.environ.get("APP_ENVIRONMENT", "")
 SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "")
 SLACK_APP_TOKEN = os.environ.get("SLACK_APP_TOKEN", "")
 PORT = os.environ.get("PORT", 8080)
-
 app = App(
     token=SLACK_BOT_TOKEN,
     process_before_response=True
@@ -16,7 +16,12 @@ app = App(
 
 def handle_mention(event, say):
     print("handle_mention")
-    say(event['text'])
+    thread_id = event['ts']
+
+    if "thread_ts" in event:
+        thread_id = event['thread_ts']
+
+    say(event['text'], thread_ts=thread_id)
 
 
 def slack_ack(ack: Ack):
